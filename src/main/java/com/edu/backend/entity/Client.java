47efa82f0677cbd334f726a5ca06c57871210d8e
skiflok <anisimov.a.v@inbox.ru>
2024-09-25 17:41:13 +0300

    develop: client

--- src/main/java/com/edu/backend/entity/Client.java
+++ src/main/java/com/edu/backend/entity/Client.java
@@ -1,10 +1,14 @@
 package com.edu.backend.entity;
 
+import com.edu.backend.enums.Gender;
 import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.time.LocalDate;
+
 @Getter
 @Entity
+@Table(name = "client", schema = "s21")
 public class Client {
 
     @Id
@@ -12,5 +16,25 @@ public class Client {
     @SequenceGenerator(name = "clientsIdSeq", sequenceName = "clients_id_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "clientsIdSeq")
     private Long id;
+
+    @Column(name = "client_name")
+    private String clientName;
+
+    @Column(name = "client_surname")
+    private String clientSurname;
+
+    @Column(name = "birthday")
+    private LocalDate birthday;
+
+    @Enumerated(EnumType.STRING) // Используйте EnumType.ORDINAL, если хотите хранить индекс
+    @Column(name = "gender")
+    private Gender gender;
+
+    @Column(name = "registration_date")
+    private LocalDate registrationDate;
+
+    // todo foreign key
+    @Column(name = "address_id")
+    private Long addressId;
 
 }
---
+++ src/main/java/com/edu/backend/enums/Gender.java
@@ -0,0 +1,6 @@
+package com.edu.backend.enums;
+
+public enum Gender {
+    MALE,
+    FEMALE
+}
