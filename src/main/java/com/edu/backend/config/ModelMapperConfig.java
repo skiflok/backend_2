b4c77f494f2a1755f3250dab45a8cdbae6fe20ed
skiflok <anisimov.a.v@inbox.ru>
2024-10-16 11:10:08 +0300

    develop: rename model mapper

--- src/main/java/com/edu/backend/config/ModelMapperConfig.java
+++ src/main/java/com/edu/backend/config/ModelMapperConfig.java
@@ -7,7 +7,7 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @Configuration
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
-public class AppConfig {
+public class ModelMapperConfig {
 
     @Bean
     public ModelMapper defaultModelMapper() {
