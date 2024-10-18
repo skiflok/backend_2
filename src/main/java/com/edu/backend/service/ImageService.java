82ed65c6bccd10201b6ff12752b1eab90956c2a8
skiflok <anisimov.a.v@inbox.ru>
2024-10-18 14:54:39 +0300

    develop: create crud

--- src/main/java/com/edu/backend/service/ImageService.java
+++ src/main/java/com/edu/backend/service/ImageService.java
@@ -1,9 +1,12 @@
 package com.edu.backend.service;
 
+import com.edu.backend.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ImageService {
+
+    private final ImageRepository imageRepository;
 }
