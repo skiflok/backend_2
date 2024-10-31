646571456c857d60bdb151202871d79a8b68a646
skiflok <anisimov.a.v@inbox.ru>
2024-10-31 07:53:38 +0300

    develop: ImageController

--- src/main/java/com/edu/backend/controller/ImageController.java
+++ src/main/java/com/edu/backend/controller/ImageController.java
@@ -1,6 +1,5 @@
 package com.edu.backend.controller;
 
-import com.edu.backend.dto.ImageDto;
 import com.edu.backend.service.ImageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
