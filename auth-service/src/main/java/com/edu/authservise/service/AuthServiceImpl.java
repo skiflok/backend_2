6b958d995c6880fb97439848733c0cab8f1e70fe
skiflok <anisimov.a.v@inbox.ru>
2025-03-12 06:48:47 +0300

    feature/auth: create method

--- auth-service/src/main/java/com/edu/authservise/service/AuthServiceImpl.java
+++ auth-service/src/main/java/com/edu/authservise/service/AuthServiceImpl.java
@@ -9,11 +9,49 @@ import net.devh.boot.grpc.server.service.GrpcService;
 @GrpcService
 public class AuthServiceImpl extends AuthServiceGrpc.AuthServiceImplBase {
     @Override
-    public void createUser(CreateUserRequest request, StreamObserver<CreateUserResponse> responseObserver) {
+    public void createUser(CreateUserRequest request, StreamObserver<JwtTokenReturn> responseObserver) {
         log.info("create user request = {}", request);
 
-        CreateUserResponse response = CreateUserResponse.newBuilder()
+        JwtTokenReturn response = JwtTokenReturn.newBuilder()
                 .setJwtToken("this is jwt token :)")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void accessCheck(AccessCheckRequest request, StreamObserver<JwtTokenReturn> responseObserver) {
+        log.debug("request = \n{}", request);
+
+        JwtTokenReturn response = JwtTokenReturn.newBuilder()
+                .setJwtToken("this is jwt token :)")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void changePassword(ChangePasswordRequest request, StreamObserver<ChangePasswordResponse> responseObserver) {
+        log.debug("request = \n{}", request);
+
+        ChangePasswordResponse response = ChangePasswordResponse.newBuilder()
+                .setSuccess(true)
+                .setMessage("password change success")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void recoverPassword(PasswordRecoveryRequest request, StreamObserver<PasswordRecoveryResponse> responseObserver) {
+        log.debug("request = \n{}", request);
+
+        PasswordRecoveryResponse response = PasswordRecoveryResponse.newBuilder()
+                .setSuccess(true)
+                .setMessage("password success recover")
                 .build();
 
         responseObserver.onNext(response);
