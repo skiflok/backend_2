0a68692aaee4258d7de09d2fbaee769749085c1f
skiflok <anisimov.a.v@inbox.ru>
2025-05-25 12:37:59 +0300

    feature/generate-service: add dto and product utils

---
+++ event-service/src/main/java/com/edu/eventservice/dto/ProductDto.java
@@ -0,0 +1,47 @@
+package com.edu.eventservice.dto;
+
+import com.edu.eventservice.enums.Category;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@Builder
+public class ProductDto {
+
+    private Long id;
+    private String name;
+    private Category category;
+    private BigDecimal price;
+    private Integer availableStock;
+    private LocalDate lastUpdateDate;
+    private Long supplierId;
+    private UUID imageId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductDto that = (ProductDto) o;
+        return Objects.equals(name, that.name) &&
+                category == that.category &&
+                Objects.equals(price, that.price) &&
+                Objects.equals(availableStock, that.availableStock) &&
+                Objects.equals(lastUpdateDate, that.lastUpdateDate) &&
+                Objects.equals(supplierId, that.supplierId) &&
+                Objects.equals(imageId, that.imageId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, category, price, availableStock, lastUpdateDate, supplierId, imageId);
+    }
+}
