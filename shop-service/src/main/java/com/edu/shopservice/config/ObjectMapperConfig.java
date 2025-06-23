499e140c585da0a6abeaa1e6d506146e46c86ca2
skiflok <anisimov.a.v@inbox.ru>
2025-06-23 08:25:37 +0300

    feature/generate-service: fix object mapper

--- shop-service/src/main/java/com/edu/shopservice/config/ObjectMapperConfig.java
+++ shop-service/src/main/java/com/edu/shopservice/config/ObjectMapperConfig.java
@@ -1,14 +1,25 @@
 package com.edu.shopservice.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class ObjectMapperConfig {
 
+//    @Bean
+//    ObjectMapper defaultObjectMapper () {
+//        return new ObjectMapper();
+//    }
+
     @Bean
-    ObjectMapper defaultObjectMapper () {
-        return new ObjectMapper();
+//    @Qualifier("defaultObjectMapper")
+    public ObjectMapper defaultObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        return builder
+                .modules(new JavaTimeModule())
+//                .featuresToEnable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+                .build();
     }
 }
