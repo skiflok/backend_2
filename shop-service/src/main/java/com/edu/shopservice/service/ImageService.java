b241425c481b72d08f39f4f29f537980bfdca173
skiflok <anisimov.a.v@inbox.ru>
2025-07-01 20:52:45 +0300

    feature/generate-service: get image by redis

--- shop-service/src/main/java/com/edu/shopservice/service/ImageService.java
+++ shop-service/src/main/java/com/edu/shopservice/service/ImageService.java
@@ -7,12 +7,13 @@ import com.edu.shopservice.repository.ProductRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.UUID;
 
 @Slf4j
@@ -22,14 +23,34 @@ public class ImageService {
 
     private final ImageRepository imageRepository;
     private final ProductRepository productRepository;
-    private final ModelMapper modelMapper;
+    private final RedisTemplate<String, byte[]> redis;
+    // TTL кеша 1 час
+    private static final Duration TTL = Duration.ofHours(1);
+
+    private String productKey(Long productId) {
+        return "product:image:" + productId + ":original";
+    }
+    private String imageKey(UUID imageId) {
+        return "image:" + imageId;
+    }
 
     public byte[] getImageById(UUID uuid) {
-        return imageRepository.findById(uuid)
+        String key = imageKey(uuid);
+        byte[] cached = redis.opsForValue().get(key);
+        if (cached != null) {
+            log.info("get image by redis [uuid = {}]", uuid);
+            return cached;
+        }
+        byte[] fresh = imageRepository.findById(uuid)
                 .map(Image::getImage)
                 .orElseThrow(() ->
-                        new EntityNotFoundException(String.format("Изображение с [id=%s] не найден", uuid))
-                );
+                        new EntityNotFoundException("Image not found: " + uuid));
+        log.info("get image by postgres [uuid = {}]", uuid);
+        if (fresh.length <= 1_000_000) {
+            redis.opsForValue().set(key, fresh, TTL);
+            log.info("set image to redis [uuid = {}]", uuid);
+        }
+        return fresh;
     }
 
     public byte[] getImageByProductId(Long id) {
